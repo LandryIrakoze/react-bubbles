f06443650164c092856dd444101ce70f074ba93e
Landry Irakoze <irakozel@gmail.com>
2019-08-23 11:30:22 -0400

    routed to bubblepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
+import './styles.scss';
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Route 
-          exact path="/" 
+          exact path='/' 
           render={props => <Login {...props}/>}    
+        />
+        <PrivateRoute
+          exact path ='/bubbles' 
+          component={BubblePage}
         />
         {/* 
           Build a PrivateRoute component that will 
